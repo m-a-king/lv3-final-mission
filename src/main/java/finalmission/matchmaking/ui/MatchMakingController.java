b610301dd6446104da296a971a46df637b75b980
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-09-03 16:40:50 +0900

    fix: 컨트롤러 엔드포인트 수정 및 메시지 개선

--- src/main/java/finalmission/matchmaking/ui/MatchMakingController.java
+++ src/main/java/finalmission/matchmaking/ui/MatchMakingController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,9 @@ public class MatchMakingController {
     private final ValidatePlayerUseCase validatePlayerUseCase;
 
     @GetMapping
-    public ResponseEntity<List<Apply>> get(@RequestBody final UserInfo userInfo) {
+    public ResponseEntity<List<Apply>> get(@RequestHeader("nickname") final String nickname,
+                                           @RequestHeader("password") final String password) {
+        final UserInfo userInfo = new UserInfo(nickname, password);
         validatePlayerUseCase.execute(userInfo);
         final List<Apply> result = getMatchMakingUseCase.execute(userInfo.nickname());
 
@@ -40,7 +43,7 @@ public class MatchMakingController {
         validatePlayerUseCase.execute(userInfo);
         joinMatchMakingUseCase.execute(userInfo.nickname());
 
-        return ResponseEntity.status(HttpStatus.CREATED).body("신청 성공");
+        return ResponseEntity.status(HttpStatus.CREATED).body("매치메이킹 신청이 완료되었습니다.");
     }
 
     @DeleteMapping
@@ -48,6 +51,6 @@ public class MatchMakingController {
         validatePlayerUseCase.execute(userInfo);
         exitMatchMakingUseCase.execute(userInfo.nickname());
 
-        return ResponseEntity.ok().body("삭제 완료");
+        return ResponseEntity.ok("매치메이킹 신청이 취소되었습니다.");
     }
 }
--- src/main/java/finalmission/player/ui/SignInController.java
+++ src/main/java/finalmission/player/ui/SignInController.java
@@ -5,18 +5,22 @@ import finalmission.player.application.SignInRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/signin")
 @RequiredArgsConstructor
 public class SignInController {
 
     private final RegisterPlayerUseCase registerPlayerUseCase;
 
+    @PostMapping
     public ResponseEntity<String> execute(@RequestBody final SignInRequest signInRequest) {
         registerPlayerUseCase.execute(signInRequest);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 잘 됐습니다");
+        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 완료되었습니다.");
     }
 }
